f7407f715c67f194a3a1380758a491647d594edc
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-12-04 14:54:24 +0300

    LinksAndOther
    C++

---
+++ Caslo.LinksAndOther.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+void SwapValue(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int varOne = 64;
+    int varTwo = 21;
+
+    cout << "Var 1 " << varOne << endl;
+    cout << "Var 2 " << varTwo << endl;
+
+    cout << "Swap" << endl;
+
+    SwapValue(varOne, varTwo);
+
+    cout << "Var 1 " << varOne << endl;
+    cout << "Var 2 " << varTwo << endl;
+}
