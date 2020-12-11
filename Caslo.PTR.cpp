3178f7bb8c4a97177d7f6ae54a1df467ace3407e
Caslo1 <71670417+Caslo1@users.noreply.github.com>
2020-12-11 14:49:18 +0300

    Smart Pointer

---
+++ Caslo.PTR.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+void lvl()
+{
+    auto_ptr<int> plvl(new int(100));
+
+    int* Inlvl = plvl.get();
+    
+    cout << "Caslo" << endl;
+
+    cout << *Inlvl << endl;
+
+};
+
+void NewLvl(const int &var)
+{
+    unique_ptr<int> current(new int(var));
+
+    cout << "Hagrit " <<*current << endl;
+}
+
+void LvlUp(const int &var)
+{
+    shared_ptr<int> lvlup(new int(var));
+    shared_ptr<int> currLvl(lvlup);
+
+    cout << "Thift" << endl;
+    cout << *currLvl << endl;
+}
+
+int main()
+{
+    int lvls;
+
+    lvl();
+
+    cout << "Galjin" << endl;
+    auto_ptr<int> lvl2(new int(21));
+    cout << *lvl2 << endl;
+
+    cin >> lvls;
+
+    LvlUp(lvls);
+
+    NewLvl(21);
+}
